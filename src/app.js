db573570d560a7b38510a3afe392deb0e2a4704a
Franco <francoherrera77003@gmail.com>
2023-02-03 18:45:40 -0300

    configure handlebars y agregue la carpeta layout con el nav, cree el formulario en index y su ruta post

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
 import express from 'express'
+import { engine } from 'express-handlebars'
 import indexRoutes from './routes/index.routes'
+import path from 'path'
 
 const app =  express()
 
+
+app.set('views', path.join(__dirname, 'views'))
+
+app.engine(
+    '.hbs',
+    engine({
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    defaultLayout: 'main',
+    extname: '.hbs',
+}))
+app.set('view engine', '.hbs')
+
+
+// Rutas
 app.use(indexRoutes)
 
 export default app
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,7 +3,11 @@ const router  = Router()
 
 
 router.get('/', (req, res) => {
-    res.send('Hola Mundo')
+    res.render('index')
+})
+
+router.post('/guardar', (req, res) => {
+    res.send('guardado!')
 })
 
 export default router
