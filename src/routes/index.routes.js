7ab95fb073cb25367cfecfcefb8c316cc22da42b
Franco <francoherrera77003@gmail.com>
2023-02-06 20:01:56 -0300

    arme las rutas post para editar los articulos y agregue la info de cada articulo en el formulario edit. ya se pueden editar los articulos

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -14,7 +14,20 @@ router.get('/about', (req, res) => {
     res.render('about')
 })
 
+router.get('/edit/:id', async (req, res) => {
 
+    try {
+        const articulo = await Articulo.findById(req.params.id).lean()
+
+        res.render('edit', {articulo})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+
+// RUTAS POST
 router.post('/guardar', async (req, res) => {
 
     let articulo = Articulo(req.body)
@@ -29,8 +42,16 @@ router.post('/guardar', async (req, res) => {
 })
 
 
-router.get('/edit', (req, res) => {
-    res.render('edit')
+router.post('/edit/:id', async (req, res) => {
+    
+    // De req.params, solo uso id
+    const {id} = req.params
+
+    await Articulo.findByIdAndUpdate(id, req.body)
+
+    res.redirect('/')
 })
+
+
 
 export default router
